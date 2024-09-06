1f8fd055d4827b88ffb504f5e1bff979c686f21b
lilleyz7 <lilley5>
2024-09-06 10:52:52 -0700

    Fix error handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, flash
 from validator import validate_card
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -20,21 +20,39 @@ def base_route():
     if request.method == 'POST':
         # handle validation
         number = request.form["cardNumber"]
-        valid, card_type = validate_card(number)
+        if number is None or len(number) < 8 or len(number) > 18:
+            flash('Please enter a valid card number')
+            return render_template('home.html')
+        valid, card_company = validate_card(number)
         if valid:
-            return render_template('home.html', submit=True, number=number, is_valid=True, ctype=card_type)
+            return render_template('home.html', submit=True, number=number, is_valid=True, ctype=card_company)
         else:
             return render_template('home.html', submit=True, number=number, is_valid=False)
 
-@app.route('/api/<card_number>', methods=['POST'])
+@app.route('/api/<card_number>', methods=['GET', 'POST'])
 def api_route(card_number):
     if card_number is None:
         error = {
             'error': 'card_number is required'
         }
         return jsonify(error)
+    if len(card_number) > 18 or len(card_number) < 8:
+        error = {
+            'error': 'invalid length of card number'
+        }
+        return jsonify(error)
     
     else:
-        return jsonify("Content-Type not supported!")
+        is_valid, card_company = validate_card(card_number)
+        if not is_valid:
+            response = {
+                'data': 'card is invalid'
+            }
+            return jsonify(response)
+        response = {
+            'data': 'card is valid',
+            'carrier': card_company
+        }
+        return jsonify(response)
 if __name__ == '__main__':
     app.run()
